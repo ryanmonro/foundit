dfe98eb6ab2664500d96d7283fd66b35eb0064b1
ryanmonro <ryan@ryanmonro.info>
2017-10-25 10:21:58 +1100

    Adding fog

---
+++ config/initializers/carrierfog.rb
@@ -0,0 +1,20 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',         
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+    }
+    config.fog_directory  = ENV['AWS_BUCKET_NAME']
+    config.fog_public     = false
+    # config.root = Rails.root.join('tmp')
+    # config.cache_dir = 'files'
+    # config.permissions = 0777
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage :file
+  end
+end
