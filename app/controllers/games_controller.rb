d2193a9b1d86dcc34cfab49c7ae7a7bf8e5ef2a3
Sidhra <sidhra.cader@hotmail.com>
2017-10-25 10:17:40 +1100

    create new games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,4 +7,16 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     @locations = @game.locations
   end
+
+  def new
+
+  end
+
+  def create
+    game = Game.new
+    game.name = params[:name]
+    game.locations = Location.all.shuffle[0..5]
+    game.save
+    redirect_to "/games"
+  end
 end
