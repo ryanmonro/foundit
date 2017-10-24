83646740bc265faa899c990e5690c0883a2dfca0
arielse <ariel@bottledup.net>
2017-10-24 16:36:14 +1100

    locations route setup and views setup

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,20 @@
 class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+    @founds = Found.all
+  end
+
+  def new
+    @locations = Location.new
+  end
+
+  def create
+
+  end
+
+
 end
