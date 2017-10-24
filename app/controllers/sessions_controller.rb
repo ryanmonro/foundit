d8d8341ed925a203140d9bdb46af8ca691c804bb
ryanmonro <ryan@ryanmonro.info>
2017-10-24 21:34:58 +1100

    Logging in works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def new
     @user = User.new
-
   end
 
   def create
@@ -11,13 +10,14 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else 
-      # @errors = @user.errors
+      @errors = "Invalid username/password combination"
       render :new
     end
   end
 
   def destroy
-
+    log_out
+    redirect_to '/'
   end
 
 end
