e174a770d7f0fe491ee5f623c2c84160b5b8d83d
arielse <ariel@bottledup.net>
2017-10-25 15:21:55 +1100

    api building

--- app/controllers/api/founds_controller.rb
+++ app/controllers/api/founds_controller.rb
@@ -1,10 +1,22 @@
 class Api::FoundsController < ApplicationController
   def create
+    @location = Location.find(params[:id])
+    game_id = GameLocation.where(location_id: @location, game_id: params[:game_id])
+    @founds = Found.where(game_location_id: game_id)
+
     found = Found.new
     found.user_id = params[:user_id]
     found.game_locations_id = params[:game_locations_id]
-    if condition
-
+    found_count = if @founds.count >= 4
+      points = 1
+    elsif @founds.count < 1
+      points = 4
+    elsif @founds.count == 2
+      points = 3
+    elsif @founds.count == 3
+      points = 2
     end
+    found.points = found_count
+    found.save
   end
 end
