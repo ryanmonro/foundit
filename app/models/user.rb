7ff539b4714a3882e8b3efdfae4038622ca947a4
ryanmonro <ryan@ryanmonro.info>
2017-10-25 17:35:13 +1100

    Displaying points for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :founds
+
+  def points
+    my_founds = Found.where(user_id: id)
+    points = 0
+    my_founds.each do |f|
+      points += f.points
+    end
+    points
+  end
 end
