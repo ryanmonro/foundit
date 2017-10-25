fec6c44c8d168ad10872fb74caa53712e699eb1e
Sidhra <sidhra.cader@hotmail.com>
2017-10-25 14:29:22 +1100

    gamelocation page

--- app/controllers/game_locations_controller.rb
+++ app/controllers/game_locations_controller.rb
@@ -1,5 +1,7 @@
 class GameLocationsController < ApplicationController
   def show
-    
+    @location = Location.find(params[:id])
+    # game = Game.findby(name: @location.name)
+    # @game_locations = game.game_locations
   end
 end
