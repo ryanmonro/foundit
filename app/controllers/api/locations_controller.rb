c522019fdd2f55adf5c56f5b3ccd20b1ca890243
arielse <ariel@bottledup.net>
2017-10-25 14:34:59 +1100

    api

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -6,7 +6,6 @@ class Api::LocationsController < ApplicationController
 
   def show
     @location = Location.find(params[:id])
-    @founds = Found.where(game_locations_id: params[:id])
     render json: @location, @founds
   end
 
