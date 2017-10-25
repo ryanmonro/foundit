fe4e58974a71fe421fe9729ab4316fb0fd351250
Sidhra <sidhra.cader@hotmail.com>
2017-10-26 09:22:00 +1100

    remove jquery script from application.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-GameLocation.destroy_all
 Found.destroy_all
+GameLocation.destroy_all
 Location.destroy_all
 Game.destroy_all
 User.destroy_all
