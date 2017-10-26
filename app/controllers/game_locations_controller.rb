9a674a6b1b5c78ee91b4173847bba6f9c6a76cab
ryanmonro <ryan@ryanmonro.info>
2017-10-26 11:44:03 +1100

    Getting proximity to game location through the API motherfuckers

--- app/controllers/game_locations_controller.rb
+++ app/controllers/game_locations_controller.rb
@@ -3,5 +3,12 @@ class GameLocationsController < ApplicationController
     @location = Location.find(params[:id])
     @game_location = GameLocation.find_by(location_id: params[:id], game_id: params[:game_id])
     @founds = Found.where(game_location_id: @game_location.id)
+    @user_has_found = false
+    if logged_in?
+      found = Found.where(game_location_id: @game_location.id, user_id: current_user.id)
+      if found.count > 0
+        @user_has_found = true
+      end
+    end
   end
 end
